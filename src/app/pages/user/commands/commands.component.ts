da9acf9f240b0f9be77f3dfe7aa8a68bee11064d
Nadia Tverdokhlib <dome.hope.tv@gmail.com>
2025-04-23 13:32:02 +0300

    add main row to the command page

--- src/app/pages/user/commands/commands.component.ts
+++ src/app/pages/user/commands/commands.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
+import { UacodeService } from 'src/app/core/services/uacode.service';
 import { UserService } from 'src/app/modules/user/services/user.service';
-import { FormInterface } from 'src/app/core/modules/form/interfaces/form.interface';
-import { FormService } from 'src/app/core/modules/form/form.service';
 
 @Component({
 	templateUrl: './commands.component.html',
@@ -9,76 +8,10 @@ import { FormService } from 'src/app/core/modules/form/form.service';
 	standalone: false,
 })
 export class CommandsComponent {
-	formDoc: FormInterface = this._form.getForm('docForm', {
-		formId: 'docForm',
-		title: 'Doc form',
-		components: [
-			{
-				name: 'Text',
-				key: 'name',
-				focused: true,
-				fields: [
-					{
-						name: 'Placeholder',
-						value: 'Enter your name',
-					},
-					{
-						name: 'Label',
-						value: 'Name',
-					},
-				],
-			},
-			{
-				name: 'Text',
-				key: 'phone',
-				fields: [
-					{
-						name: 'Placeholder',
-						value: 'Enter your phone',
-					},
-					{
-						name: 'Label',
-						value: 'Phone',
-					},
-				],
-			},
-			{
-				name: 'Text',
-				key: 'bio',
-				fields: [
-					{
-						name: 'Placeholder',
-						value: 'Enter your bio',
-					},
-					{
-						name: 'Label',
-						value: 'Bio',
-					},
-					{
-						name: 'Textarea',
-						value: true,
-					},
-				],
-			},
-			{
-				name: 'Button',
-				fields: [
-					{
-						name: 'Label',
-						value: "Let's go",
-					},
-					{
-						name: 'Submit',
-						value: true,
-					},
-				],
-			},
-		],
-	});
 
 	isMenuOpen = false;
 
-	constructor(public userService: UserService, private _form: FormService) {}
+	constructor(public userService: UserService, public uaCodeServise: UacodeService) {}
 
 	back(): void {
 		window.history.back();
